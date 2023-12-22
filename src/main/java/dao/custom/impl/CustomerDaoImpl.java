e49a1e4ffa9a9b0a4681dfee962cfad7ad8b33da
SINESH2004 <138645911+SINESH2004@users.noreply.github.com>
2023-12-22 17:30:25 +0530

    Save , Update and Delete Customer by hibernate

--- src/main/java/dao/custom/impl/CustomerDaoImpl.java
+++ src/main/java/dao/custom/impl/CustomerDaoImpl.java
@@ -6,6 +6,10 @@ import db.DBConnection;
 import dto.CustomerDto;
 import dto.ProductsDto;
 import entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,20 +20,44 @@ import java.util.List;
 public class CustomerDaoImpl implements CustomerDao {
     @Override
     public boolean save(Customer entity) throws SQLException, ClassNotFoundException {
-        String sql = "INSERT INTO customer VALUES(?,?,?,?)";
-        return CrudUtil.execute(sql,entity.getId(),entity.getName(),entity.getAddress(),entity.getSalary());
+        Configuration configuration = new Configuration().
+                configure("hibernate.cfg.xml").
+                addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(entity);
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
     public boolean update(Customer entity) throws SQLException, ClassNotFoundException {
-        String sql = "UPDATE customer SET name=?, address=?, salary=? WHERE id=?";
-        return CrudUtil.execute(sql,entity.getName(),entity.getAddress(),entity.getSalary(),entity.getId());
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Customer customer = session.find(Customer.class, entity.getId());
+        customer.setName(entity.getName());
+        customer.setAddress(entity.getAddress());
+        customer.setSalary(entity.getSalary());
+        session.save(customer);
+        transaction.commit();
+        return true;
     }
 
     @Override
     public boolean delete(String value) throws SQLException, ClassNotFoundException {
-        String sql = "DELETE from customer WHERE id=?";
-        return CrudUtil.execute(sql,value);
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Customer.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(session.find(Customer.class,value));
+        transaction.commit();
+        return true;
     }
 
     @Override
--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -1,11 +1,17 @@
 package entity;
 import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Customer {
+    @Id
     private String id;
     private String name;
     private String address;
